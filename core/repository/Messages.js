3973c853b556568c7a04c77ac89d275dd5d937ac
Jose J. Pardines <info@josejpg.com>
2019-05-21 23:34:04 +0200

    Update Messages

--- core/repository/Messages.js
+++ core/repository/Messages.js
@@ -1,15 +1,13 @@
 // Requires
-const db = require('../services/DB');
+const db = require( '../services/DB' );
 
 /**
  * Find all by event
  * @param params
  */
-const findByEvent = (params) => {
-
-    let sql = `SELECT DISTINCT m.id, m.ref_evento as evento, m.ref_proveedor as proveedor, m.ref_cliente as cliente, m.mensaje FROM proyectobd.mensajes m WHERE m.ref_evento = ${params.idEvent}`;
-    return db.query(sql);
-
+const findByEvent = ( params ) => {
+	let sql = `SELECT DISTINCT m.id, m.ref_evento as evento, m.ref_proveedor as proveedor, m.ref_cliente as cliente, m.mensaje FROM proyectobd.mensajes m WHERE m.ref_evento = ${ params.evento.id }`;
+	return db.query( sql );
 };
 
 
@@ -17,24 +15,25 @@ const findByEvent = (params) => {
  * Save a new service
  * @param params
  */
-const save = (params) => {
-    const columns = [];
-    const values = [];
-    columns.push('ref_evento');
-    values.push(`'${params.idEvent}'`);
-    columns.push('mensaje');
-    values.push(`'${params.message}'`);
-    if (params.idProvider != null) {
-        columns.push('ref_proveedor');
-        values.push(`'${params.idProvider}'`);
-    } else {
-        columns.push('ref_cliente');
-        values.push(`'${params.idClient}'`);
-    }
-    columns.push('fecha');
-    values.push(`'${new Date().getTime()}'`);
-    let sql = `INSERT INTO proyectobd.mensajes ( ${columns.join(',')} ) VALUES ( ${values.join(',')} )`;
-    return db.query(sql);
+const save = ( params ) => {
+	const columns = [];
+	const values = [];
+	columns.push( 'ref_evento' );
+	values.push( `'${ params.evento.id }'` );
+	columns.push( 'mensaje' );
+	values.push( `'${ params.mensaje }'` );
+	if ( params.proveedor != null &&
+		 params.proveedor.id != null ) {
+		columns.push( 'ref_proveedor' );
+		values.push( `'${ params.proveedor.id }'` );
+	} else {
+		columns.push( 'ref_cliente' );
+		values.push( `'${ params.cliente.id }'` );
+	}
+	columns.push( 'fecha' );
+	values.push( `'${ new Date().getTime() }'` );
+	let sql = `INSERT INTO proyectobd.mensajes ( ${ columns.join( ',' ) } ) VALUES ( ${ values.join( ',' ) } )`;
+	return db.query( sql );
 };
 
-module.exports = {db, findByEvent, save};
+module.exports = { db, findByEvent, save };
--- core/routes/Messages.js
+++ core/routes/Messages.js
@@ -1,30 +1,30 @@
 // Requires
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require( 'express' );
+const bodyParser = require( 'body-parser' );
 
 
 // Repository
-const Events = require('../repository/Events');
-const Providers = require('../repository/Providers');
-const Clients = require('../repository/Clients');
-const Messages = require('../repository/Messages');
+const Events = require( '../repository/Events' );
+const Providers = require( '../repository/Providers' );
+const Clients = require( '../repository/Clients' );
+const Messages = require( '../repository/Messages' );
 
 // Token
-const Token = require('../services/Token');
+const Token = require( '../services/Token' );
 
 // Config
 const app = express();
-app.use(bodyParser.json({
-    limit: '50mb',
-    extended: true,
-    type: 'application/json'
-}));
-app.use(bodyParser.urlencoded({
-    limit: '50mb',
-    extended: true,
-    parameterLimit: 50000,
-    type: 'application/x-www-form-urlencoding'
-}));
+app.use( bodyParser.json( {
+	limit: '50mb',
+	extended: true,
+	type: 'application/json'
+} ) );
+app.use( bodyParser.urlencoded( {
+	limit: '50mb',
+	extended: true,
+	parameterLimit: 50000,
+	type: 'application/x-www-form-urlencoding'
+} ) );
 const router = express.Router();
 
 
@@ -35,50 +35,40 @@ const router = express.Router();
  * Response Error: { "ok": Boolean, "error": String }
  *
  */
-router.post('/', (req, res) => {
-
-    if (req.headers['authorization'] != null) {
-        const token = req.headers['authorization'].replace('Bearer ', '');
-        const dataToken = Token.validateToken(token);
-
-        if (dataToken) {
-
-            if (dataToken.exp < new Date().getTime()) {
-                Messages.save(req.body)
-                    .then(() => {
-                        return {
-                            ok: true
-                        };
-                    })
-                    .then(message => {
-                        res.status(200).send(message);
-                    })
-                    .catch(err => {
-                        let data = {ok: false, error: "Message couldn't be registered"};
-                        console.log(err);
-                        res.status(400).send(data);
-                    });
-            } else {
-
-                let data = {ok: false, error: "Token expired"};
-                res.status(403).send(data);
-
-            }
-        } else {
-
-            let data = {ok: false, error: "Token is not correct"};
-            res.status(403).send(data);
-
-        }
-    } else {
-
-        let data = {ok: false, error: "Token is not correct"};
-        res.status(403).send(data);
-
-    }
-
-});
-
+router.post( '/', ( req, res ) => {
+	if ( req.headers[ 'authorization' ] != null ) {
+		const token = req.headers[ 'authorization' ].replace( 'Bearer ', '' );
+		const dataToken = Token.validateToken( token );
+		if ( dataToken ) {
+			if ( dataToken.exp < new Date().getTime() ) {
+				Messages
+					.save( req.body )
+					.then( () => {
+						return {
+							ok: true
+						};
+					} )
+					.then( message => {
+						res.status( 200 ).send( message );
+					} )
+					.catch( err => {
+						let data = { ok: false, error: "Message couldn't be registered" };
+						console.log( err );
+						res.status( 400 ).send( data );
+					} );
+			} else {
+				let data = { ok: false, error: "Token expired" };
+				res.status( 403 ).send( data );
+			}
+		} else {
+			let data = { ok: false, error: "Token is not correct" };
+			res.status( 403 ).send( data );
+		}
+	} else {
+		let data = { ok: false, error: "Token is not correct" };
+		res.status( 403 ).send( data );
+	}
+} );
 
 /**
  * GET: Messages.
@@ -86,100 +76,89 @@ router.post('/', (req, res) => {
  * Response Error: { "ok": Boolean, "error": String }
  *
  */
-router.get('/:_idEvent', (req, res) => {
-
-    if (req.headers['authorization'] != null) {
-        const token = req.headers['authorization'].replace('Bearer ', '');
-        const dataToken = Token.validateToken(token);
-
-        if (dataToken) {
-
-            if (dataToken.exp < new Date().getTime()) {
-                const params = {};
-                if (req.params._idEvent != null) {
-                    params.idEvent = req.params._idEvent;
-                }
-                Messages.findByEvent(params)
-                    .then(listMessages => {
-                        const listPromises = [];
-                        listMessages.map(dataMessage => {
-                            listPromises.push( Events.findById(dataMessage.evento)
-                                .then(dataEvent => {
-                                    dataMessage.evento = dataEvent;
-                                    return dataMessage;
-                                })
-                                .then(dataMessage => {
-                                    if (dataMessage.proveedor != null) {
-                                        return Providers.findById(dataMessage.proveedor)
-                                            .then(dataProvider => {
-                                                dataMessage.proveedor = dataProvider[ 0 ];
-                                                if (dataMessage.proveedor.email === dataToken.user) {
-                                                    dataMessage.owner = true;
-                                                }
-                                                return dataMessage;
-                                            });
-                                    } else {
-                                        delete dataMessage.proveedor;
-                                        return dataMessage;
-                                    }
-                                })
-                                .then(dataMessage => {
-                                    if (dataMessage.cliente != null) {
-                                        return Clients.findById(dataMessage.cliente)
-                                            .then(dataClient => {
-                                                dataMessage.cliente = dataClient[ 0 ];
-                                                console.log( dataMessage.cliente.email );
-                                                console.log( dataToken.user );
-                                                if (dataMessage.cliente.email === dataToken.user) {
-                                                    dataMessage.owner = true;
-                                                }
-                                                return dataMessage;
-                                            });
-                                    } else {
-                                        delete dataMessage.cliente;
-                                        return dataMessage;
-                                    }
-                                })
-                            );
-                        });
-                        return Promise.all( listPromises ).then( result => result );
-                    })
-                    .then(listMessages => {
-                        return {
-                            ok: true,
-                            messages: listMessages,
-                        };
-                    })
-                    .then((message) => {
-                        res.status(200).send(message);
-                    })
-                    .catch(err => {
-                        let data = {ok: false, error: "Error recovering service. Try again in a few minutes"};
-                        console.log(err);
-                        res.status(400).send(data)
-                    });
-
-
-            } else {
-
-                let data = {ok: false, error: "Token expired"};
-                res.status(403).send(data);
-
-            }
-
-        } else {
-
-            let data = {ok: false, error: "Token is not correct"};
-            res.status(403).send(data);
-
-        }
-    } else {
-
-        let data = {ok: false, error: "Token is not correct"};
-        res.status(403).send(data);
-
-    }
-
-});
+router.get( '/:_idEvent', ( req, res ) => {
+	if ( req.headers[ 'authorization' ] != null ) {
+		const token = req.headers[ 'authorization' ].replace( 'Bearer ', '' );
+		const dataToken = Token.validateToken( token );
+		if ( dataToken ) {
+			if ( dataToken.exp < new Date().getTime() ) {
+				const params = {};
+				params.evento = {};
+				params.evento.id = req.params._idEvent;
+				Messages
+					.findByEvent( params )
+					.then( listMessages => {
+						const listPromises = [];
+						listMessages.map( dataMessage => {
+							listPromises.push(
+								Events
+									.findById( dataMessage.evento )
+									.then( dataEvent => {
+										dataMessage.evento = dataEvent[ 0 ];
+										return dataMessage;
+									} )
+									.then( dataMessage => {
+										if ( dataMessage.proveedor != null ) {
+											return Providers
+												.findById( dataMessage.proveedor )
+												.then( dataProvider => {
+													dataMessage.proveedor = dataProvider[ 0 ];
+													if ( dataMessage.proveedor.email === dataToken.user ) {
+														dataMessage.owner = true;
+													}
+													return dataMessage;
+												} );
+										} else {
+											delete dataMessage.proveedor;
+											return dataMessage;
+										}
+									} )
+									.then( dataMessage => {
+										if ( dataMessage.cliente != null ) {
+											return Clients
+												.findById( dataMessage.cliente )
+												.then( dataClient => {
+													dataMessage.cliente = dataClient[ 0 ];
+													if ( dataMessage.cliente.email === dataToken.user ) {
+														dataMessage.owner = true;
+													}
+													return dataMessage;
+												} );
+										} else {
+											delete dataMessage.cliente;
+											return dataMessage;
+										}
+									} )
+							);
+						} );
+						return Promise.all( listPromises ).then( result => result );
+					} )
+					.then( listMessages => {
+						return {
+							ok: true,
+							messages: listMessages,
+						};
+					} )
+					.then( ( message ) => {
+						res.status( 200 ).send( message );
+					} )
+					.catch( err => {
+						let data = { ok: false, error: "Error recovering service. Try again in a few minutes" };
+						console.log( err );
+						res.status( 400 ).send( data )
+					} );
+			} else {
+				let data = { ok: false, error: "Token expired" };
+				res.status( 403 ).send( data );
+			}
+		} else {
+			let data = { ok: false, error: "Token is not correct" };
+			res.status( 403 ).send( data );
+		}
+	} else {
+		let data = { ok: false, error: "Token is not correct" };
+		res.status( 403 ).send( data );
+	}
+} );
 
 module.exports = router;
